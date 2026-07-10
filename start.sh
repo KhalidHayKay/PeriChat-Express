d466af0754fb807b9f18933b8aa7b9f036078268
Alamutu Khalid <alamutukhalid@gmail.com>
2026-07-10 16:16:52 +0100

    fix: production entyrypoint

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ if [ "$APP_ENV" = "local" ]; then
     yarn install --frozen-lockfile
     exec ./node_modules/.bin/tsx watch src/server.ts
 else
-    exec node dist/server.js
+    exec node dist/src/server.js
 fi
